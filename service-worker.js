aa0aa61348bb99c36e1cd6a81952e05c1b4001b4
VicenteVicente <vecalisto@uc.cl>
2022-05-08 21:35:03 -0400

    fix: changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-console.log('Service worker script loaded!')
+console.log('Service worker script loaded!');
 
 /**
  * The cache name should change every time you want to "cache bust"
@@ -7,48 +7,46 @@ console.log('Service worker script loaded!')
  * e.g. Vite or Next.js would hash the files so the name is unique based on content
  * (style-XYZ123.css etc)
  */
-const CACHE_NAME = "pwa-grupo-8-v1";
+const CACHE_NAME = 'pwa-grupo-8-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "global.css",
-  "icon.svg",
-  "logo.png",
-  "apple-touch-icon-180x180.png"
+  '/',
+  '/index.html',
+  'global.css',
+  'icon.svg',
+  'logo.png',
+  'favicon.png',
+  'apple-touch-icon-180x180.png',
 ];
 
-
-self.addEventListener("install", async (event) => {
-  console.log("installing!");
+self.addEventListener('install', async (event) => {
+  console.log('installing!');
   const cache = await self.caches.open(CACHE_NAME);
   await cache.addAll(urlsToCache);
   self.skipWaiting();
 });
 
-
-self.addEventListener("activate", async (event) => {
-  console.log("activating!");
+self.addEventListener('activate', async (event) => {
+  console.log('activating!');
   const keys = await caches.keys();
-  keys.filter(key => key !== CACHE_NAME)
-      .map(key => event.waitUntil(caches.delete(key)))
+  keys
+    .filter((key) => key !== CACHE_NAME)
+    .map((key) => event.waitUntil(caches.delete(key)));
 });
-
 
 async function cacheFirst(request) {
   const cache = await self.caches.open(CACHE_NAME);
   try {
     const response = await fetch(request);
     cache.put(request, response.clone());
-    console.log("Network: ", request.url);
+    console.log('Network: ', request.url);
     return response;
   } catch {
-    console.log("Cache: ", request.url);
+    console.log('Cache: ', request.url);
     return cache.match(request);
   }
 }
 
-
-self.addEventListener("fetch", event => {
-  if (event.request.method != "GET") return;
+self.addEventListener('fetch', (event) => {
+  if (event.request.method != 'GET') return;
   event.respondWith(cacheFirst(event.request));
 });
